3d278bbecf86fce62b0d4c02f287cde05e247c9e
gks01 <gks01@home>
2022-03-18 23:22:48 +0900

    개인과제 운동기록 프로그램

--- jave2022wed/src/practice/exercise_record.java
+++ jave2022wed/src/practice/exercise_record.java
@@ -1,3 +1,4 @@
+package practice;
 import java.util.Scanner;
 
 public class exercise_record {
@@ -8,7 +9,7 @@ public class exercise_record {
 		
 		String[][] mem = new String[100][4];// id /pw /이름 /불륨  : 회원 2차원배열
 		String[][] bor = new String[100][5];// id / 이름 / 횟수 / 세트 / 무게 : 기록 2차원배열 
-		
+		int 이전불륨 = 0;
 		
 		while (true) {// 프로그램실행
 			try {
@@ -55,11 +56,11 @@ public class exercise_record {
 							}
 						}
 					}// 중복체크 for문 종료
-					
+					if(pass == false) {System.out.println("로그인실패");}
 					while (pass) {// 로그인시 메뉴 무한반복
 						System.out.println("***** 운동기록 *****");
 						//출력
-						System.out.println("이름\t\t횟수\t\t세트\t\t총kg");
+						System.out.println("이름\t\t횟수\t\t세트\t\t1회무게");
 						for(int i = 0; i < bor.length ; i++) { // bor길이 만큼 반복
 							if(bor[i][0] != null && bor[i][0].equals(id) ) { // id 값이 일치하면 기록 보여주기
 								System.out.printf("%s\t\t%s\t\t%s\t\t%s\n", bor[i][1],bor[i][2],bor[i][3],bor[i][4]);
@@ -74,7 +75,12 @@ public class exercise_record {
 							System.out.println("횟수: ");	String count = s.next();// 횟수입력받기
 							System.out.println("세트: ");	String set = s.next();// 세트입력받기
 							System.out.println("1회무게: ");  String weight = s.next();// 무게입력받기
-							//기록저장
+							//기록저장 총합 무게를 정하기 위해 int형으로 변환
+							int 횟수 = Integer.parseInt(count);
+							int 세트 = Integer.parseInt(set);
+							int 무게 = Integer.parseInt(weight);
+							int 총합 = (무게 * 횟수) * 세트;
+							
 							for(int i = 0; i < bor.length ; i++) { // bor 길이만큼 반복
 								if(bor[i][0] == null) { // null 이라면
 									bor[i][0] = id;	//저장
@@ -83,18 +89,14 @@ public class exercise_record {
 									bor[i][3] = set;
 									bor[i][4] = weight;
 									
-									int ss = Integer.parseInt(weight);	// 총불륨을 저장하기위해 int 로 변환							
-									for(int j = 0; j < mem.length ; j++) { // id 동일한 mem 찾기
-										if(mem[j][0] != null && mem[j][0].equals(id)) {// id 동일한 meme 찾기
-											int add = Integer.parseInt(bor[i][4]); // mem 안에있는 무게 int 변환
-											int sum = ss + add;						// 합해서 저장
-											mem[j][3] = Integer.toString(sum);	// String 으로 변환후 저장
-											break;
-										}
-									}
 									break;
 								}
 							}// 기록저장 end
+							for(int i = 0; i < bor.length ; i++) {
+								if(mem[i][0] != null && mem[i][0].equals(id)) {
+									mem[i][3] = Integer.toString(총합);
+								}
+							}
 						}else if(ch2 == 2) {// 운동 삭제
 							System.out.println("***** 운동삭제 *****");
 							System.out.println("운동이름: "); String name = s.next(); // 이름입력받기
@@ -112,11 +114,34 @@ public class exercise_record {
 							}// 기록삭제 end
 						}else if(ch2 == 3) {// 운동 종료
 							System.out.println("***** 운동 종료 *****");
+							
 							for(int i = 0; i < mem.length ; i++) { // mem 길이만큼 반복
 								if(mem[i][0] != null && mem[i][0].equals(id)) { // 동일한 id 같 찾기
 									System.out.println(mem[i][2] + "님의 총운동 불륨은 : " + mem[i][3] + "Kg"); // 출력
-								}
-							}
+									int 현재불륨 = Integer.parseInt(mem[i][3]); // int형변환 
+									if(현재불륨 > 이전불륨) {
+										System.out.println("어제보다" +(현재불륨 - 이전불륨)+"kg 성장하셨습니다");
+									}else if(현재불륨 < 이전불륨) {
+										System.out.println("어제보다" +(현재불륨 - 이전불륨)+"kg 도태되셨습니다");
+									}
+									
+									이전불륨 = Integer.parseInt(mem[i][3]); //현재불륨과 이전 불륨을 비교하기우ㅣ해 저장
+									mem[i][3] = null;						//다음에 현재불륨 의 저장하기위해 무게의 해당하는ㄴ 인덱스 초기화
+									// 얼마나 성장했는지 뜨고 bor 배열 초기화 
+									for(int j = 0; j < bor.length ; j++) { // bor 길이만큼 반복
+										if(bor[j][0] != null && bor[j][0].equals(id)) { // 동일한 id 의 운동이있으면
+											
+											bor[j][0] = null; //초기화
+											bor[j][1] = null;
+											bor[j][2] = null;
+											bor[j][3] = null;
+											bor[j][4] = null;
+		
+										}
+									}
+								}					
+							}// 운동종료 for 문 end
+							
 						}else if(ch2 == 4) {// 로그아웃
 							break;
 						}else {
